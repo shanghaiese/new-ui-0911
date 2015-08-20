4d58fca741d2e381a262725b6c7fa65596ef436f
Tang,Hejun <hejun.t.tang@intel.com>
2015-08-20 09:20:24 +0800

    add loading when there's a resolve in new page route; left position text in dropdown and right position pointer;

--- app/main/environment/vm/vm.controller.js
+++ app/main/environment/vm/vm.controller.js
@@ -1,32 +1,32 @@
-(function(){
+(function() {
     angular.module('ilab')
         .controller('VMCtrl', VMCtrl);
 
-    VMCtrl.$inject = ['environment', 'vm'];
-    function VMCtrl(environment, vm) {
+    VMCtrl.$inject = ['environment', 'vm', 'data'];
+
+    function VMCtrl(environment, vm, data) {
         var that = this;
         that.item = 1;
 
         that.check = function(item) {
-        	if(item === 1) {
-        		return true;
-        	}else {
-        		return false;
-        	}
-        	if(item === 1) {
-        		return true;
-        	}else {
-        		return false;
-        	}
+            if (item === 1) {
+                return true;
+            } else {
+                return false;
+            }
+            if (item === 1) {
+                return true;
+            } else {
+                return false;
+            }
         };
 
-        // console.log(environment.get('2067601'));
-        environment.getList({expand: 'virtualMachines'}).then(function(envs) {
-            that.envs = envs;
-            vm.get(envs[0].virtualMachines[0].id).then(function(data) {
-                console.log(data);
-            });
+        console.log(data);
+        that.envs = data;
+        vm.get(that.envs[0].virtualMachines[0].id).then(function(data1) {
+            console.log(data1);
         });
+
     }
 
 
