d16c4e0f51bcdc2a7d6f5dd494f0815ac86a13b2
Tang,Hejun <hejun.t.tang@intel.com>
2015-08-25 19:32:25 +0800

    complete nested named view research

--- app/main/ilab.config.js
+++ app/main/ilab.config.js
@@ -6,7 +6,8 @@
         .config(restangular)
         .config(pagination)
         .run(attachMenu)
-        .run(loading);
+        .run(loading)
+        .run(init);
 
     angular
         .module('ilabConfig')
@@ -17,76 +18,176 @@
 
     function route($stateProvider, $urlRouterProvider) {
         // For any unmatched url, redirect to /environment
-        $urlRouterProvider.otherwise("/");
+        $urlRouterProvider.otherwise("/404");
         //
         // Now set up the states
         $stateProvider
+            .state('404', {
+                url: "/404",
+                views: {
+                    'page@': {
+                        templateUrl: "404.html",
+                    }
+                }
+            })
             .state('default', {
                 url: "/",
                 templateUrl: "main/welcome/welcome.html",
                 controller: 'WelcomeCtrl',
                 controllerAs: 'Welcome'
             })
-            .state('env', {
-                url: "/environment/:envId",
-                templateUrl: "main/environment/env.html",
-                controller: 'EnvCtrl',
-                controllerAs: 'Env',
-                resolve: {
-                    // _env: function(environmentService, $stateParams) {
-                    //     return environmentService.get($stateParams.envId, {expand: 'virtualMachines,physicalMachines'});
-                    // }
-                    _env: function(_envs, $stateParams) {
-                        return _envs.get($stateParams.envId, {expand: 'virtualMachines,physicalMachines'});
-                    }
-                }
-            })
-            .state('env.vm', {
-                url: "/vm",
-                templateUrl: "main/environment/vm/vm.html",
-                controller: 'VmCtrl',
-                controllerAs: 'Vm',
-                resolve: {
-                    _vms: function(_env) {
-                        return _env.virtualMachines;
-                    }
-                }
-            })
-            .state('env.pm', {
-                url: "/pm",
-                templateUrl: "main/environment/pm/pm.html",
-                controller: 'PmCtrl',
-                controllerAs: "Pm"
-            })
-            .state('envBasic', {
-                url: "/environment-basic/:envId",
-                templateUrl: "main/environment/envBasic/envBasic.html",
-                controller: 'EnvCtrl',
-                controllerAs: 'EnvBasic',
-                resolve: {
-                    _env: function(environmentService, $stateParams) {
-                        return environmentService.get($stateParams.envId, {expand: 'virtualMachines'});
-                    }
-                }
-            })
             .state('envs', {
+                abstract: true,
+                views: {
+                    'page@': {
+                        templateUrl: 'main/templates/common.tpl.html',
+                        controller: function($rootScope) {
+                            $rootScope.page.title = "My Environment";
+                        }
+                    }
+                },
+                controller: function() {
+                    console.log('data');
+                },
+                breadcrumb: {
+                    proxy: 'envs.list'
+                }
+            })
+            .state('envs.list', {
                 url: '/environments',
-                templateUrl: "main/environment/envs.html",
-                controller: 'EnvsCtrl',
-                controllerAs: 'Envs',
+                views: {
+                    'content@envs': {
+                        templateUrl: "main/environment/envs.html",
+                        controller: 'EnvsCtrl',
+                        controllerAs: 'Envs'
+                    }
+                },
                 resolve: {
                     _envs: function(environmentService) {
                         return environmentService.getList({expand: 'virtualMachines,physicalMachines,networks,users'});
                     }
+                },
+                breadcrumb: {
+                    name: 'All environments'
+                }
+            })
+            .state('envs.detail', {
+                abstract: true,
+                url: "/environment/:envId",
+                views: {
+                    'content@envs': {
+                        templateUrl: "main/environment/env.html",
+                        controller: 'EnvCtrl',
+                        controllerAs: 'Env'
+                    }
+                },
+                resolve: {
+                    _env: function(environmentService, $stateParams) {
+                        return environmentService.get($stateParams.envId, {expand: 'virtualMachines,physicalMachines'});
+                    }
+                },
+                breadcrumb: {
+                    name: false
+                }            
+            })
+            .state('envs.detail.vm', {
+                url: "/vm",
+                views: {
+                    'tab@envs.detail': {
+                        templateUrl: "main/environment/vm/vm.html",
+                        controller: 'VmCtrl',
+                        controllerAs: 'Vm'
+                    }
+                },
+                resolve: {
+                    _vms: function(_env) {
+                        console.log(_env.virtualMachines);
+                        return _env.virtualMachines;
+                    }
+                },
+                breadcrumb: {
+                    name: '{{_env.name}}'
+                }            
+            })
+            .state('envs.detail.pm', {
+                url: "/pm",
+                views: {
+                    'tab@envs.detail': {
+                        templateUrl: "main/environment/pm/pm.html",
+                        controller: 'PmCtrl',
+                        controllerAs: "Pm"
+                    }
+                },
+                resolve: {
+                    _pms: function(_env) {
+                        console.log(_env.name);
+                        return _env.physicalMachines;
+                    }
+                },
+                breadcrumb: {
+                    name: '{{_env.name}}'
+                }
+            })
+            .state('envs.detail.pm.add', {
+                url: "/add",
+                views: {
+                    'content@envs': {
+                        template: "<h2>add pm</h2>"
+                    }
+                },
+                resolve: {
+                    _pms: function(_env) {
+                        console.log(_env.name);
+                        return _env.physicalMachines;
+                    }
+                },
+                breadcrumb: {
+                    name: 'add pm'
+                }
+            })
+            .state('envs.basic', {
+                url: "/environment-basic/:envId",
+                views: {
+                    'content@envs': {
+                        templateUrl: "main/environment/envBasic/envBasic.html",
+                        controller: 'EnvCtrl',
+                        controllerAs: 'EnvBasic'
+                    }
+                },
+                resolve: {
+                    _env: function(environmentService, $stateParams) {
+                        return environmentService.get($stateParams.envId, {expand: 'virtualMachines'});
+                    }
+                },
+                breadcrumb: {
+                    name: 'Basic View'
                 }
             })
             .state('lab', {
+                abstract: true,
                 url: "/lab",
-                templateUrl: "main/lab/lab.html"
+                views: {
+                    'page@': {
+                        templateUrl: "main/templates/common.tpl.html",
+                        controller: function($rootScope) {
+                            $rootScope.page.title = "Lab Workspace";
+                        }
+                    }
+                },
+                breadcrumb: {
+                    proxy: 'lab.list'
+                }
             })
             .state('lab.list', {
                 url: "/list",
-                templateUrl: "main/lab/lab.list.html"
+                views: {
+                    'content@lab': {
+                        templateUrl: "main/lab/lab.list.html"
+                    }
+                },
+                breadcrumb: {
+                    name: 'Lab Workspace'
+                }
             });
     }
 
@@ -134,4 +235,11 @@
         paginationTemplateProvider.setPath('main/templates/pagination.tpl.html');
     }
 
+    init.$inject = ['$rootScope'];
+
+    function init($rootScope) {
+        $rootScope.page = {
+            title: 'default page title'
+        };
+    }
 })();
